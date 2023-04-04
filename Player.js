6099a1219862de773ef762f2666d87bb81e9bfdd
barco <93612733+Profleide@users.noreply.github.com>
2023-04-03 21:57:24 -0300

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,83 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.rank = 0; // para armazenar a classificação dos jogadores.
+    this.fuel = 185; // para atribuir o combustível inicial ao jogador; vamos reduzi-lo
+    //quando o carro estiver em movimento.
+    this.life = 185;//para atribuir vida inicial ao jogador, vamos reduzi-la nas próximas
+    //aulas
+    this.score = 0; // para armazenar a pontuação
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      score: this.score
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+  // Aluno Função Classificação dos jogadores
+  getCarsAtEnd() {
+    database.ref("carsAtEnd").on("value", data => {
+      this.rank = data.val();
+    });
+  }
+  // Aluno 
+  static updateCarsAtEnd(rank) {
+    database.ref("/").update({
+      carsAtEnd: rank
+    });
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 41--pro
+# C41RV_SpeedRacer_ReferenceCode
+Reference Code
